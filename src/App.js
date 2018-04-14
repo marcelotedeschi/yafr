93e5d1f3d80371fab81f89660b5710de5fa5b034
caioblasio <caioblasio@Caios-MacBook-Air.local>
2018-04-14 13:52:48 -0300

    Adds Redux, React Router and Redux Promise

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="container">
-  <div className="row">
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>
+        <div className="card" style={{width: '18rem'}}>
+          <img className="card-img-top" src="..." alt="Card image cap"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import reducers from './reducers';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+//Switch só renderiza o primeiro route que encontrar. Assim, tem que colocar as rotas mais especificas primeiro e a home por ultimo
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+   <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/opa" component={App} />
+        </Switch>
+      </div>
+   </BrowserRouter>
+  </Provider>
+  , document.querySelector('#root'));
 
