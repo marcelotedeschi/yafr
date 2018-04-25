8f805c0b4d1d7f97c1189b929ccc8ccd79544f93
Marcelo Tedeschi <marceloftedeschi@gmail.com>
2018-04-25 11:26:29 -0300

    Remove rss-to-json in favour of axios + xml2js
    As discussed, having the request separate from the parser allows us much more control in the future
    such as, but not limited to:
    - specific parsing logic to our needs
    - multiple parsers
    - concurrent requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,20 @@
-import Feed from 'rss-to-json';
+import axios from 'axios';
+import { parseString as parseXML } from 'xml2js';
 
 export const FETCH_FEED = 'FETCH_FEED';
 
 export function fetchFeed(callback) {
   const request = new Promise((resolve, reject) => {
-    Feed.load('http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml', function(err, rss) {
-      if (rss) {
-        resolve(rss);
-      }
-    })
+    axios.get('http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml')
+      .then((response) => {
+        parseXML(response.data, (err, result) => {
+          resolve(result.rss.channel[0]);
+        });
+      })
+      .catch((error) => {
+        // TODO: Decide how to handle errors (show notification?)
+        reject(error);
+      });
   });
 
   return { type: FETCH_FEED, payload: request }
